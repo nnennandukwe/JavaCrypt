da74027f91cac7e9d775614cb5e7cd27722c8faf
Nnenna Ndukwe <nnenna.s.ndukwe@gmail.com>
2018-12-11 19:21:31 -0500

    basic shell of json and chain builder with sha256 encryption

--- src/com/company/Block.java
+++ src/com/company/Block.java
@@ -1,4 +1,30 @@
 package com.company;
 
+import java.util.Date;
+
 public class Block {
+
+    public String hash;
+	public String previousHash;
+	private String data;
+	private long timeStamp; //as number of milliseconds since 1/1/1970
+
+
+    // CONSTRUCTOR
+	public Block(String data, String previousHash ) {
+		this.data = data;
+		this.previousHash = previousHash;
+        this.timeStamp = new Date().getTime();
+        this.hash = genHash();
+    }
+
+    public String genHash() {
+        String generatedHash = StringDoctor.applySha256( 
+                previousHash +
+                Long.toString(timeStamp) +
+                data 
+                );
+        return generatedHash;
+    }
+    
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,21 @@
 package com.company;
 
+import java.util.ArrayList;
+import com.google.gson.*;
+
+
 public class Main {
 
+    public static ArrayList<Block> chain = new ArrayList<Block>();
+
     public static void main(String[] args) {
-	// write your code here
+
+        chain.add(new Block("This is a brand new block 1", "0"));
+
+        String chainJson = new GsonBuilder().setPrettyPrinting().create().toJson(chain);
+        System.out.println(chainJson);
+
     }
+
+
 }
